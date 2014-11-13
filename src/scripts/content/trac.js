4d33b7e011a7480da94c058efcbe3fb8cac47538
Myles Loffler <myles@britecore.com>
2014-11-12 21:10:59 -0600

    Tweaking Selectors for Button

--- src/scripts/content/trac.js
+++ src/scripts/content/trac.js
@@ -3,20 +3,22 @@
 
 'use strict';
 
-togglbutton.render('#main > #content.ticket:not(.toggl)', {observe: true}, function (elem) {
+togglbutton.render('#main #content.ticket:not(.toggl)', {observe: true}, function (elem) {
   var
     link,
     tracTicketId = (
+      $('#trac-ticket-title > a') || // BriteCore Trac
       $('#main > #content.ticket > #ticket > h2 > .trac-id') || // Trac v1.x
-      $('#main > #content.ticket > h1#trac-ticket-title > a') // Trac v0.x 
+      $('#main > #content.ticket > h1#trac-ticket-title > a') // Trac v0.x
     ).innerHTML,
-    tracTicketDescription = $('#main > #content.ticket > #ticket .summary', elem).textContent,
+    tracTicketDescription = $('#main #content.ticket #ticket .summary', elem).textContent,
     tracProjectName = (
       $('title').textContent.split('     – ').pop() || // First try to get project name from title tag
       $('#banner > #header > #logo > img').attr('alt') // If can't find in title tag, get from logo alt attribute
     ),
     container = (
       $('#main > #content.ticket > #ticket > h2 > .trac-type', elem) || // Trac v1.x
+      $('#trac-ticket-title > a') ||
       $('#main > #content.ticket > h1#trac-ticket-title > a', elem) // Trac v0.x
     ),
     spanTag;
